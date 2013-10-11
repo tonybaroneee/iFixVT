c653a75533c5782f6555ea11811fe8c6000e11ff
Keith Buel <keith.buel@gmail.com>
2013-10-11 18:52:14 -0400

    added in spring

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,14 +4,23 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 import views.html.heatmap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 
-public class Application extends Controller {
+import play.mvc.Result;
+import views.html.index;
 
-    public static Result index() {
-        return ok(index.render("Your mom's new application is ready."));
-    }
+@Controller
+public class Application extends FixItBaseController {
 
-    public static Result heatmap() {
+	private static Logger _logger = LoggerFactory.getLogger(Application.class);
+
+	public Result index() {
+        	return ok(index.render("Your mom's new application is ready."));
+	}
+
+    public Result heatmap() {
         return ok(heatmap.render());
     }
 }
--- build.sbt
+++ build.sbt
@@ -2,10 +2,18 @@ name := "iFixVT"
 
 version := "1.0-SNAPSHOT"
 
+val springVersion = "3.2.0.RELEASE"
+
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
-  cache
+  cache,
+  "org.slf4j" % "jcl-over-slf4j" % "1.6.6",
+  "javax.inject" % "javax.inject" % "1",
+  "cglib" % "cglib" % "2.2.2",
+  "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.9",
+  "org.springframework" % "spring-context" % springVersion,
+  "org.springframework" % "spring-expression" % springVersion
 )     
 
 play.Project.playJavaSettings
