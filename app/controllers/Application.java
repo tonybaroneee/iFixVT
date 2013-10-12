dc2a3ad10ca41c830ff9c61e80bd1d09607b26c5
Keith Buel <keith.buel@gmail.com>
2013-10-11 20:30:08 -0400

    added mongo db

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,18 +9,27 @@ import org.springframework.stereotype.Controller;
 import play.mvc.Result;
 import views.html.heatmap;
 import views.html.index;
-
+import db.models.Issue;
+import db.service.IssueService;
 
 @Controller
 public class Application extends FixItBaseController {
 
 	private static Logger _logger = LoggerFactory.getLogger(Application.class);
 
+	@Inject
+	IssueService _issueService;
+
 	public Result index() {
-        	return ok(index.render("Your mom's new application is ready."));
+		
+		Issue issue = new Issue();
+		issue.setImageUri("MY URI!!!!");
+		_issueService.save(issue);
+		
+        return ok(index.render("Your mom's new application is ready."));
 	}
 
-    public Result heatmap() {
-        return ok(heatmap.render());
-    }
+	public Result heatmap() {
+		return ok(heatmap.render());
+	}
 }
--- app/db/models/Issue.java
+++ app/db/models/Issue.java
@@ -1,35 +1,51 @@
 package db.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@TypeAlias("Issue")
 public class Issue {
+
+	@Id
+	private String id;
+
 	private IssueType issueType;
 	private Double latitude;
 	private Double longitude;
 	private String imageUri;
-	
+
 	public Double getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(Double longitude) {
 		this.longitude = longitude;
 	}
+
 	public String getImageUri() {
 		return imageUri;
 	}
+
 	public void setImageUri(String imageUri) {
 		this.imageUri = imageUri;
 	}
+
 	public IssueType getIssueType() {
 		return issueType;
 	}
+
 	public void setIssueType(IssueType issueType) {
 		this.issueType = issueType;
 	}
+
 	public Double getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}
-	
-	
+
 }
